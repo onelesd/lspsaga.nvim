b335e458ca5f68a7bc0b724cdb61388255e8dc40
onelesd <958603+onelesd@users.noreply.github.com>
2023-07-11 13:26:57 -0700

    Update init.lua

--- lua/lspsaga/symbol/init.lua
+++ lua/lspsaga/symbol/init.lua
@@ -168,6 +168,8 @@ function symbol:register_module()
       end
 
       local client = lsp.get_client_by_id(args.data.client_id)
+      print(string.format('hi dave: s', vim.inspect(args)))
+
       if not client.supports_method('textDocument/documentSymbol') then
         return
       end
